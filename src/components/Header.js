5cfc3a0d0aef70ec0738f6b0219fa255293b9b78
Naomi <nani.torres.serrano@outlook.com>
2022-02-04 03:07:43 -0500

    this bug again

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../style/style.css';
+import img from '../img/IMG_6370 copy.jpg'
 
 function Header() {
     return(
@@ -9,19 +10,22 @@ function Header() {
     
           <div class="profile">
             {/* <img src="../img/IMG_6370 copy.jpg" alt="" class="img-fluid rounded-circle"> */}
+            <img class='imga'src={img} alt="Img"/>
             <h1 class="text-light"><a href="index.html">Naomi Torres</a></h1>
             <div class="social-links mt-3 text-center">
               <a href="https://www.instagram.com/nani.ee/" class="instagram"><i class="bx bxl-instagram"></i></a>
               <a href="https://www.linkedin.com/in/naomi-torres-837657217/" class="linkedin"><i class="bx bxl-linkedin"></i></a>
               <a href="https://github.com/Naomi969" class="instagram"><i class="bx bxl-github"></i></a>
             </div>
-          {/* </img> */}
+            {/* </img> */}
+            
           </div>
           
     
           <nav id="navbar" class="nav-menu navbar">
             <ul>
-              <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
+              <li>
+                <a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
               <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>About</span></a></li>
               <li><a href="#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Portfolio</span></a></li>
               <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
@@ -35,8 +39,9 @@ function Header() {
       {/* <!-- ======= Hero Section ======= --> */}
       <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
         <div class="hero-container" data-aos="fade-in">
-          <h1>Naomi Torres</h1>
-          <p>I'm <span class="typed" data-typed-items="Developer, Freelancer, Student"></span></p>
+          <h1>Naomi Torres idk what to write</h1>
+          {/* <p>I'm <span class="typed" data-typed-items="Developer, Freelancer, Student"></span></p> */}
+          <p className="line-1 anim-typewriter">and this is my portfolio...</p>
         </div>
       </section>
       </section>
