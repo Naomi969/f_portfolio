091e3091d192fd7e388a530f24165ad447155a46
Naomi <nani.torres.serrano@outlook.com>
2022-02-10 10:35:33 -0500

    layout of overall website is done

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,95 +5,54 @@ import '../style/style.css';
 
 function Main(){
     return (
-      <section class="about">
-      <div class="container">
-        <div class="row ">
-          <div  class="image col-xl-5 d-flex align-items-stretch justify-content-center justify-content-lg-start"></div>
-          <div class="col-xl-7 ps-20 ps-lg-5 pe-lg-1 d-flex align-items-stretch justify-content-center">
-            <div class="content d-flex flex-column justify-content-center">
-              <div class="section-title">
-                <h2>A Little Bit About Me...</h2>
-                <p>
-                I am a dedicated Software Developer with a Full Stack Web Development Certificate from University of North Carolina along with substantial experience in teaching, operational excellence, production management, and HR administration. I offer a vision-driven approach to decision making, strategic planning, and tactical implementation that I am excited to bring to your team.
-                  <br></br><br></br>
-                  Throughout my career, I have made it my priority to utilize current technologies and new techniques to drive elegant, creative solutions across a range of projects. Comfortable in both collaborative and independent roles, I offer refined analytical and critical thinking skills, and I can adapt and revise my strategies to meet evolving priorities, shifting needs, and emergent issues. I have proficiency in HTML5, CSS3, JavaScript, jQuery, Express.js, React, Angular, TypeScript, SASS, Node.js, progressive web apps, agile methodology, computer science, database theory, MongoDB, MySQL, and Git.
-                  <br></br><br></br>
-                  When I am not coding, you will find me with an iced coffee in hand, enjoying time with my husband,
-                  daughter, and two dogs! If we are not outside soaking up the sun, we are playing games or finding
-                  fun projects around the house to complete.
-                </p>
-              </div>
-              <div class="section-title" id="skills">
-                <div class="resume-section-content">
-                  <h2>Skills</h2>
-                  <div class="subheading mb-3">Programming Languages & Tools</div>
-                  <ul class="list-inline dev-icons">
-                    <li class="list-inline-item"><i class="fab fa-html5"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-angular"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-react"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-sass"></i></li>
-                    <li class="list-inline-item"><i class="fas fa-database"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-npm"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-bootstrap"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-git"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-github"></i></li>
-                    <li class="list-inline-item"><i class="fab fa-gitlab"></i></li>
+        <section id='main'>
+          <section id="about" class="about">
+            <div class="container">
 
-                  </ul>
-                  <div class="subheading mb-3">Workflow</div>
-                  <ul class="fa-ul">
-                    <li>
-                      <span class="fa-li"><i class="fas fa-check"></i></span>
-                      Mobile-First, Responsive Design
-                    </li>
-                    <li>
-                      <span class="fa-li"><i class="fas fa-check"></i></span>
-                      Pair Programming
-                    </li>
-                    <li>
-                      <span class="fa-li"><i class="fas fa-check"></i></span>
-                      Cross Functional Teams 
-                    </li>
-                    <li>
-                      <span class="fa-li"><i class="fas fa-check"></i></span>
-                      REST APIs
-                    </li>
-                    <li>
-                      <span class="fa-li"><i class="fas fa-check"></i></span>
-                      Object-Oriented-Programming, OOP
-                  </li>
-                  <li>
-                      <span class="fa-li"><i class="fas fa-check"></i></span>
-                      CRUD Methods, Request Methods
-                  </li>
-                  <li>
-                    <span class="fa-li"><i class="fas fa-check"></i></span>
-                    Jest, Express, Sequelize, Inquirer
-                  </li>
-                  <li>
-                    <span class="fa-li"><i class="fas fa-check"></i></span>
-                    Test & Debugging Skills
-                </li>
-                  <li>
-                      <span class="fa-li"><i class="fas fa-check"></i></span>
-                      Agile Mindset
-                  </li>
-                  <li>
-                    <span class="fa-li"><i class="fas fa-check"></i></span>
-                    Results-Oriented, Ready to Learn
-                </li>
-                  </ul>
+              <div class="section-title">
+                <h2>About</h2>
+                <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+              </div>
+
+              <div class="row">
+                <div class="col-lg-4" data-aos="fade-right">
+                  {/* <img src="assets/img/profile-img.jpg" class="img-fluid" alt=""> */}
+                </div>
+                <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+                  <h3>UI/UX Designer &amp; Web Developer.</h3>
+                  <p class="fst-italic">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                    magna aliqua.
+                  </p>
+                  <div class="row">
+                    <div class="col-lg-6">
+                      <ul>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
+                        <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
+                      </ul>
+                    </div>
+                    <div class="col-lg-6">
+                      <ul>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>30</span></li>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master</span></li>
+                        <li><i class="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>email@example.com</span></li>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                      </ul>
+                    </div>
+                  </div>
+                  <p>
+                    Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
+                    Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
+                  </p>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    )
-}
 
-export default Main; 
+            </div>
+          </section>
+        </section>
+        )
+        }
+
+        export default Main; 
